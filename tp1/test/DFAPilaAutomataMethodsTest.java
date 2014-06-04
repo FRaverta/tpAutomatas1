07e8860f521850a5c46a44b380ba6b35e844e1ae
cesarcorne <cesarmcornejo@gmail.com>
2014-06-04 18:14:44 -0300

    changes in DFAPila and add some tests

--- tp1/test/DFAPilaAutomataMethodsTest.java
+++ tp1/test/DFAPilaAutomataMethodsTest.java
@@ -1,46 +1,56 @@
 
 import automata.AP;
-import automata.DFA;
 import automata.DFAPila;
-import automata.FA;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
  * @author cesar
  */
 public class DFAPilaAutomataMethodsTest {
-public static String ruta ="";
 
-@Test
-	public void test1() throws Exception {
-		DFAPila dfapila = (DFAPila) AP.parse_form_file("test/DFAPIla.dot");
-		assertTrue(dfapila.accepts("abbb"));
-	}
-
-	@Test
-	public void test2() throws Exception {
-		DFAPila dfapila = (DFAPila) AP.parse_form_file("test/DFAPIla.dot");
-		assertFalse(dfapila.accepts("abaa"));
-	}
-        
-        @Test
-        public void test3() throws Exception {
-		DFAPila dfapila = (DFAPila) AP.parse_form_file("test/DFAPIla.dot");
-		assertFalse(dfapila.accepts("aaabbba"));
-	}
+    @Test
+    public void test1() throws Exception {
+	DFAPila dfapila = (DFAPila) AP.parse_form_file("test/DFAPIla.dot");
 	
-	@Test
-	public void test4() throws Exception {
-		DFAPila dfapila = (DFAPila) AP.parse_form_file("test/DFAPIla.dot");
-		assertTrue(dfapila.accepts("aaaaabbb"));
-	}
+        assertTrue(dfapila.accepts("aaaaaaaaaaaaaaaaaaaaab"));
+                
+        assertFalse(dfapila.accepts("bbbb"));
+                
+        assertTrue(dfapila.accepts("ab"));
+                
+        assertFalse(dfapila.accepts("aaabbba"));
+                
+        assertFalse(dfapila.accepts("abaa"));
+                
+        assertFalse(dfapila.accepts("aaaaabbbbbbbbb"));
+                
+    }   
+
+    @Test	
+    public void test2() throws Exception {
+        DFAPila dfapila = (DFAPila) AP.parse_form_file("test/dfapila2.dot");
+		
+        assertTrue(dfapila.accepts("aaaaaaaaaaaaaaaaaaaaab"));
+                
+        assertTrue(dfapila.accepts("aaabb"));
+                
+        assertFalse(dfapila.accepts("aaabbba"));
+                
+        assertFalse(dfapila.accepts("abaa"));
+                
+        assertFalse(dfapila.accepts("bbbb"));
+              
+        assertFalse(dfapila.accepts("aaaaabbbbbbbbbbbbbbbbbbb"));
+                
+        assertTrue(dfapila.accepts("aaaabbb"));
+                
+        assertTrue(dfapila.accepts("aaaaabbbbb"));
+                
+    } 
 
 }
